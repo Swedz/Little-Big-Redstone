f427cce91efb227fe3cd14e46f074b019b0ca552
Swedz <me@swedz.net>
2025-01-18 06:51:10 -0500

    Only use 2 variables in logic gate algebra tooltips

--- src/main/java/net/swedz/little_big_redstone/LBRText.java
+++ src/main/java/net/swedz/little_big_redstone/LBRText.java
@@ -5,12 +5,12 @@ import net.swedz.tesseract.neoforge.tooltip.TranslatableTextEnum;
 public enum LBRText implements TranslatableTextEnum
 {
 	LOGIC_GATE_ALGEBRA("Q = %s"),
-	LOGIC_GATE_ALGEBRA_AND("A \u2227 B \u2227 C"),
-	LOGIC_GATE_ALGEBRA_NAND("A \u2191 B \u2191 C"),
-	LOGIC_GATE_ALGEBRA_NOR("A \u2193 B \u2193 C"),
+	LOGIC_GATE_ALGEBRA_AND("A \u2227 B"),
+	LOGIC_GATE_ALGEBRA_NAND("A \u2191 B"),
+	LOGIC_GATE_ALGEBRA_NOR("A \u2193 B"),
 	LOGIC_GATE_ALGEBRA_NOT("\u00ACA"),
-	LOGIC_GATE_ALGEBRA_OR("A \u2228 B \u2228 C"),
-	LOGIC_GATE_ALGEBRA_XOR("A \u22BB B \u22BB C");
+	LOGIC_GATE_ALGEBRA_OR("A \u2228 B"),
+	LOGIC_GATE_ALGEBRA_XOR("A \u22BB B");
 	
 	private final String englishText;
 	
