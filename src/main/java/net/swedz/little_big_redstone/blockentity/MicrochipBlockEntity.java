4044ea199f877f362389783847b653021188fa56
Swedz <me@swedz.net>
2025-01-17 20:18:51 -0500

    Store microchip data on block entity

--- src/main/java/net/swedz/little_big_redstone/blockentity/MicrochipBlockEntity.java
+++ src/main/java/net/swedz/little_big_redstone/blockentity/MicrochipBlockEntity.java
@@ -3,6 +3,8 @@ package net.swedz.little_big_redstone.blockentity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtOps;
+import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Inventory;
@@ -12,12 +14,20 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.swedz.little_big_redstone.LBRBlocks;
 import net.swedz.little_big_redstone.gui.microchip.MicrochipMenu;
+import net.swedz.little_big_redstone.microchip.Microchip;
 
 public final class MicrochipBlockEntity extends BlockEntity implements MenuProvider
 {
+	private Microchip microchip;
+	
 	public MicrochipBlockEntity(BlockPos pos, BlockState blockState)
 	{
 		super(LBRBlocks.MICROCHIP_ENTITY.get(), pos, blockState);
+	}
+	
+	public Microchip microchip()
+	{
+		return microchip;
 	}
 	
 	@Override
@@ -36,11 +46,18 @@ public final class MicrochipBlockEntity extends BlockEntity implements MenuProvi
 	protected void loadAdditional(CompoundTag tag, HolderLookup.Provider registries)
 	{
 		super.loadAdditional(tag, registries);
+		
+		if(tag.contains("microchip", Tag.TAG_COMPOUND))
+		{
+			Microchip.CODEC.parse(NbtOps.INSTANCE, tag.getCompound("microchip")).ifSuccess((success) -> microchip = success);
+		}
 	}
 	
 	@Override
 	protected void saveAdditional(CompoundTag tag, HolderLookup.Provider registries)
 	{
 		super.saveAdditional(tag, registries);
+		
+		tag.put("microchip", Microchip.CODEC.encodeStart(NbtOps.INSTANCE, microchip).getOrThrow());
 	}
 }
