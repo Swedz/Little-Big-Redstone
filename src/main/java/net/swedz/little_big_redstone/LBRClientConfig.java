98b68ec69129769ff661e5dfe6dacb594198a28f
Swedz <me@swedz.net>
2025-11-02 17:19:44 -0500

    Add client config option for scale of sticky note tooltip view (#104)

--- src/main/java/net/swedz/little_big_redstone/LBRClientConfig.java
+++ src/main/java/net/swedz/little_big_redstone/LBRClientConfig.java
@@ -25,6 +25,17 @@ public interface LBRClientConfig
 		return 0.5;
 	}
 	
+	@ConfigKey("sticky_note_tooltip_view_scale")
+	@ConfigComment({
+			"The scale to apply to the tooltip sticky note view on the screen.",
+			"1 results in the sticky note view being 180x180."
+	})
+	@Range.Double(min = 0, max = 4)
+	default double stickyNoteTooltipViewScale()
+	{
+		return 0.5;
+	}
+	
 	@ConfigKey("sticky_note_in_world_view_linger_time")
 	@ConfigComment("The amount of time (in ticks) sticky notes remain on the screen before fading away.")
 	@Range.Integer(min = 0, max = Integer.MAX_VALUE)
