9751fd1c91197602bfa8c5b8a79decb2b6f694c1
Swedz <me@swedz.net>
2026-05-10 17:45:43 -0400

    Better energy compatibility for the Reader

--- src/main/java/net/swedz/little_big_redstone/microchip/awareness/types/capability/EnergyCapabilityAwareness.java
+++ src/main/java/net/swedz/little_big_redstone/microchip/awareness/types/capability/EnergyCapabilityAwareness.java
@@ -1,15 +1,29 @@
 package net.swedz.little_big_redstone.microchip.awareness.types.capability;
 
-import net.neoforged.neoforge.capabilities.Capabilities;
+import net.minecraft.core.Direction;
+import net.neoforged.neoforge.capabilities.BlockCapability;
 import net.neoforged.neoforge.energy.IEnergyStorage;
 import net.swedz.little_big_redstone.microchip.awareness.AwarenessType;
 import net.swedz.little_big_redstone.microchip.awareness.AwarenessTypes;
+
+import java.util.List;
 
 public final class EnergyCapabilityAwareness extends CapabilityAwareness<EnergyCapabilityAwareness, IEnergyStorage>
 {
 	public EnergyCapabilityAwareness()
 	{
-		super(Capabilities.EnergyStorage.BLOCK);
+		super();
+	}
+	
+	@Override
+	protected List<? extends BlockCapability<IEnergyStorage, Direction>> getCapabilities()
+	{
+		return BlockCapability.getAll().stream()
+				.filter((capability) ->
+						IEnergyStorage.class.isAssignableFrom(capability.typeClass()) &&
+						capability.contextClass() == Direction.class)
+				.map((capability) -> (BlockCapability<IEnergyStorage, Direction>) capability)
+				.toList();
 	}
 	
 	@Override
