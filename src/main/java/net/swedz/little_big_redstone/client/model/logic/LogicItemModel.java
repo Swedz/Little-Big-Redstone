9c787006c9189ff5354d5441cbb96abd9c5e25d5
Swedz <me@swedz.net>
2026-06-04 10:39:41 -0400

    Move RenderTypedItemLayerKey to tesseract

--- src/main/java/net/swedz/little_big_redstone/client/model/logic/LogicItemModel.java
+++ src/main/java/net/swedz/little_big_redstone/client/model/logic/LogicItemModel.java
@@ -1,7 +1,6 @@
 package net.swedz.little_big_redstone.client.model.logic;
 
 import com.google.common.collect.Lists;
-import com.mojang.math.Quadrant;
 import com.mojang.math.Transformation;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
@@ -9,23 +8,14 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.block.dispatch.BlockModelRotation;
 import net.minecraft.client.renderer.block.dispatch.ModelState;
-import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
 import net.minecraft.client.renderer.item.CuboidItemModelWrapper;
 import net.minecraft.client.renderer.item.ItemModel;
 import net.minecraft.client.renderer.item.ItemModelResolver;
 import net.minecraft.client.renderer.item.ItemStackRenderState;
 import net.minecraft.client.renderer.item.ModelRenderProperties;
-import net.minecraft.client.renderer.rendertype.RenderType;
-import net.minecraft.client.resources.model.ModelBaker;
 import net.minecraft.client.resources.model.ModelDebugName;
-import net.minecraft.client.resources.model.cuboid.CuboidFace;
-import net.minecraft.client.resources.model.cuboid.FaceBakery;
-import net.minecraft.client.resources.model.cuboid.ItemModelGenerator;
 import net.minecraft.client.resources.model.cuboid.ItemTransforms;
-import net.minecraft.client.resources.model.geometry.BakedQuad;
-import net.minecraft.client.resources.model.geometry.QuadCollection;
 import net.minecraft.client.resources.model.sprite.Material;
-import net.minecraft.core.Direction;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.entity.ItemOwner;
 import net.minecraft.world.item.DyeColor;
@@ -38,8 +28,8 @@ import net.swedz.little_big_redstone.LBR;
 import net.swedz.little_big_redstone.LBRItemDisplayContext;
 import net.swedz.little_big_redstone.item.LogicItem;
 import net.swedz.little_big_redstone.microchip.object.logic.LogicType;
+import net.swedz.tesseract.neoforge.model.RenderTypedItemLayerKey;
 import org.joml.Matrix4fc;
-import org.joml.Vector3f;
 
 import java.util.List;
 import java.util.Optional;
@@ -98,57 +88,8 @@ public record LogicItemModel(
 		var overlayRenderProperties = new ModelRenderProperties(false, overlayMaterial, itemTransforms);
 		// TODO 26.1 logic scanline render type
 		var renderType = /*index == 2 ? LBRClientRenderTypes.logicScanline() : */NeoForgeRenderTypes.ITEM_UNSORTED_TRANSLUCENT.get();
-		var overlayQuads = baker.compute(new ItemLayerKey(overlayMaterial, MODEL_STATE, index, renderType, extraFaceData));
+		var overlayQuads = baker.compute(new RenderTypedItemLayerKey(overlayMaterial, MODEL_STATE, index, renderType, extraFaceData));
 		return new CuboidItemModelWrapper(List.of(), overlayQuads, overlayRenderProperties, transformation);
-	}
-	
-	// TODO 26.1 move to tesseract?
-	private record ItemLayerKey(
-			Material.Baked material,
-			ModelState modelState,
-			int layerIndex,
-			RenderType renderType,
-			ExtraFaceData faceData
-	) implements ModelBaker.SharedOperationKey<QuadCollection>
-	{
-		@Override
-		public QuadCollection compute(ModelBaker modelBakery)
-		{
-			var builder = new QuadCollection.Builder();
-			var sprite = material.sprite();
-			var transparency = sprite.transparency();
-			var materialInfo = new BakedQuad.MaterialInfo(
-					sprite,
-					ChunkSectionLayer.byTransparency(transparency),
-					renderType,
-					layerIndex,
-					true,
-					faceData.lightEmission(),
-					faceData.ambientOcclusion()
-			);
-			bakeExtrudedSprite(builder, modelBakery.interner(), modelState, modelBakery.interner().materialInfo(materialInfo), faceData);
-			return builder.build();
-		}
-	}
-	
-	// TODO 26.1 remove these
-	private static final CuboidFace.UVs SOUTH_FACE_UVS = new CuboidFace.UVs(0.0F, 0.0F, 16.0F, 16.0F);
-	private static final CuboidFace.UVs NORTH_FACE_UVS = new CuboidFace.UVs(16.0F, 0.0F, 0.0F, 16.0F);
-	
-	// TODO 26.1 access transform in ItemModelGenerator
-	private static void bakeExtrudedSprite(
-			QuadCollection.Builder builder,
-			ModelBaker.Interner interner,
-			ModelState modelState,
-			BakedQuad.MaterialInfo materialInfo,
-			net.neoforged.neoforge.client.model.ExtraFaceData faceData
-	)
-	{
-		Vector3f from = new Vector3f(0.0F, 0.0F, 7.5F);
-		Vector3f to = new Vector3f(16.0F, 16.0F, 8.5F);
-		builder.addUnculledFace(FaceBakery.bakeQuad(interner, from, to, SOUTH_FACE_UVS, Quadrant.R0, materialInfo, Direction.SOUTH, modelState, null, faceData));
-		builder.addUnculledFace(FaceBakery.bakeQuad(interner, from, to, NORTH_FACE_UVS, Quadrant.R0, materialInfo, Direction.NORTH, modelState, null, faceData));
-		ItemModelGenerator.bakeSideFaces(builder, interner, modelState, materialInfo, faceData);
 	}
 	
 	public record Unbaked(
