9576fae0b07b2387ac0a613682ee290605682647
Swedz <me@swedz.net>
2026-06-08 22:50:07 -0400

    Hacky fix for scanline moving faster with larger atlases

--- src/main/resources/assets/little_big_redstone/shaders/include/logic_scanline.glsl
+++ src/main/resources/assets/little_big_redstone/shaders/include/logic_scanline.glsl
@@ -5,6 +5,7 @@
 float logic_scanline(vec2 texCoord0, ivec2 textureSize)
 {
     vec2 motion = vec2(0, 5);
+    motion *= vec2(1024, 512) / textureSize;
     vec2 translation = vec2(GameTime) * motion;
     vec2 coord = (texCoord0 + translation) * textureSize;
     float lineScale = 3;
