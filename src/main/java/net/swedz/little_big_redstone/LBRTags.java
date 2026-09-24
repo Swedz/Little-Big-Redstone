7c9dee0caf8a9d339972f5d40b22dbf25e2a53c2
Swedz <me@swedz.net>
2026-09-24 19:38:54 -0400

    Port to 26.1.2: Allow sticky notes in logic array

--- src/main/java/net/swedz/little_big_redstone/LBRTags.java
+++ src/main/java/net/swedz/little_big_redstone/LBRTags.java
@@ -19,6 +19,8 @@ public final class LBRTags
 		public static final TagKey<Item> LOGIC_ARRAYS     = TagKey.create(Registries.ITEM, LBR.id("logic_arrays"));
 		public static final TagKey<Item> FLOPPY_DISKS     = TagKey.create(Registries.ITEM, LBR.id("floppy_disks"));
 		public static final TagKey<Item> STICKY_NOTES     = TagKey.create(Registries.ITEM, LBR.id("sticky_notes"));
+		
+		public static final TagKey<Item> LOGIC_ARRAY_CONTAINS = TagKey.create(Registries.ITEM, LBR.id("logic_array_contains"));
 	}
 	
 	public static final class Blocks
--- src/main/java/net/swedz/little_big_redstone/datagen/server/provider/tags/ItemTagDatagenProvider.java
+++ src/main/java/net/swedz/little_big_redstone/datagen/server/provider/tags/ItemTagDatagenProvider.java
@@ -2,6 +2,7 @@ package net.swedz.little_big_redstone.datagen.server.provider.tags;
 
 import net.minecraft.core.HolderLookup;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.neoforged.neoforge.common.Tags;
@@ -9,6 +10,7 @@ import net.neoforged.neoforge.common.data.ItemTagsProvider;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 import net.swedz.little_big_redstone.LBR;
 import net.swedz.little_big_redstone.LBRItems;
+import net.swedz.little_big_redstone.LBRLogicTypes;
 import net.swedz.little_big_redstone.LBRTags;
 import net.swedz.tesseract.neoforge.registry.holder.ItemHolder;
 
@@ -37,6 +39,24 @@ public final class ItemTagDatagenProvider extends ItemTagsProvider
 				.add(Items.HONEYCOMB);
 	}
 	
+	private void logicArrayContains()
+	{
+		this.tag(LBRTags.Items.LOGIC_ARRAY_CONTAINS)
+				.add(LBRItems.REDSTONE_BIT.asItem());
+		
+		for(var type : LBRLogicTypes.values())
+		{
+			this.tag(LBRTags.Items.LOGIC_ARRAY_CONTAINS)
+					.add(type.item());
+		}
+		
+		for(var color : DyeColor.values())
+		{
+			this.tag(LBRTags.Items.LOGIC_ARRAY_CONTAINS)
+					.add(LBRItems.stickyNote(color).asItem());
+		}
+	}
+	
 	@Override
 	protected void addTags(HolderLookup.Provider provider)
 	{
@@ -50,6 +70,7 @@ public final class ItemTagDatagenProvider extends ItemTagsProvider
 		
 		this.dyeWasher();
 		this.sealant();
+		this.logicArrayContains();
 	}
 	
 	@Override
